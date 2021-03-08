6268737b2a2c554c9c144a325179c02502cb62b0
JEFF <jeffreyjohnens@gmail.com>
2021-03-08 08:54:17 -0800

    update

--- button.py
+++ button.py
@@ -17,16 +17,24 @@ def create_button():
     -d BUTTONSUBTYPE=PRODUCTS \
     -d BUTTONCOUNTRY=US \
     -d L_BUTTONVAR1=item_name%3Dshoehorn \
-    -d L_BUTTONVAR2=amount%3D1464.46 \
-    -d L_BUTTONVAR3=tax=%3D21 \
-    -d L_BUTTONVAR4=item_number%3D123456
+    -d L_BUTTONVAR2=tax=%3D21 \
+    -d L_BUTTONVAR3=item_number%3D123456 \
+    -d OPTION0NAME=Color \
+    -d L_OPTION0SELECT0=Red \
+    -d L_OPTION0SELECT1=Blue \
+    -d L_OPTION0SELECT2=Green \
+    -d L_OPTION0PRICE0=10.00 \
+    -d L_OPTION0PRICE1=8.00 \
+    -d L_OPTION0PRICE2=12.00
   """
 
   output = check_output(command,shell=True)
   output = output.decode('utf-8')
+  print(output)
   output = output.split('WEBSITECODE=')[1].split("&EMAILLINK")[0]
   html = unquote(output).strip()
   return html
 
 
 html = create_button()
+print(html)
