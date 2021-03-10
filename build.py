d7c4883a5929158c9c2a60d9dfde49a58ef8bf17
JEFF <jeffreyjohnens@gmail.com>
2021-03-09 23:02:06 -0800

    update

--- build.py
+++ build.py
@@ -1,5 +1,8 @@
 import os
 import yaml
+import json
+import copy
+from itertools import product
 from PIL import Image
 from button import create_button
 
@@ -12,6 +15,9 @@ product_id: {product_id}
 
 with open("_data/products.yml") as file:
   products = yaml.load(file, Loader=yaml.FullLoader)
+
+with open("_data/clasps.yml") as file:
+  clasps = yaml.load(file, Loader=yaml.FullLoader)["clasps"]
 
 # build the product pages
 os.makedirs("product_pages", exist_ok=True)
@@ -42,9 +48,11 @@ def convert_image(path):
   im.thumbnail((1000,1000))
   im.save(folder + path)
 
-
+all_options = {}
+buttons = {}
 items = []
-for k,v in products.items():
+for product_num,(k,v) in enumerate(products.items()):
+  print("BUILDING ", product_num)
   item = {
     "image_path" : v["images"][0]["path"],
     "image_url" : "product/{}".format(k),
@@ -60,18 +68,30 @@ for k,v in products.items():
   #  if image.get("video",None) is None:
   #    convert_image(image["path"])
   
-  # make the buttons
-  # combine the options
-  options = copy.deepcopy(v["options"])
-  if len(options) == 0:
-    options = {"name":"", "price":"0.00"}
-  
-  
-  
-  
+  # make the button
+  BLANK_OPTION = [{"value":"-", "price":"0.00"}]
+  sizes = copy.deepcopy(v.get("sizes",BLANK_OPTION))
+  styles = copy.deepcopy(v.get("styles",BLANK_OPTION))
 
-  exit()
-  print( create_button( options ) )
+  merged_options = []
+  merged_options_yml = {}
+  for ii,opts in enumerate(product(sizes,styles,clasps)):
+    value = "&".join([str(opt["value"]) for opt in opts])
+    price = float(v["price"]) + sum([float(opt["price"]) for opt in opts])
+    price = "{:.2f}".format(price)
+    merged_options.append({"value":ii,"price":price})
+    merged_options_yml[value] = {"value":ii,"price":price}
+
+  buttons[product_num+1] = create_button(merged_options)
+  all_options[product_num+1] = json.dumps(merged_options_yml)
+
+
+# write buttons to a yaml
+with open("_data/paypal_buttons.yml", "w") as f:
+  yaml.dump(buttons, f)
+
+with open('_data/merged_options.yml', 'w') as f:
+  yaml.dump(all_options, f)
 
 itemsets = [items[i:i+3] for i in range(0,len(items),3)]
 content = {"row" + str(i) : itemset for i,itemset in enumerate(itemsets)}
--- button.py
+++ button.py
@@ -1,12 +1,12 @@
 from subprocess import check_output
 from urllib.parse import unquote
 
-def add_option(label, options, index, key="name"):
+def add_option(label, options, index, **kwargs):
 
   cmd = """-d OPTION{index}NAME={label} """.format(index=index, label=label)
 
   for ii,option in enumerate(options):
-    cmd += """-d L_OPTION{index}SELECT{ii}='{value}' """.format(index=index, ii=ii, value=option[key])
+    cmd += """-d L_OPTION{index}SELECT{ii}={value} """.format(index=index, ii=ii, value=option["value"])
   for ii,option in enumerate(options):
     cmd += """-d L_OPTION{index}PRICE{ii}={price} """.format(index=index, ii=ii, price=option["price"])
 
@@ -33,11 +33,9 @@ curl https://api-3t.sandbox.paypal.com/nvp \
 """
 
   command += add_option("Options", options, 0)
-
   output = check_output(command,shell=True)
-  output = output.decode('utf-8')
-  #print(unquote(output))
-  output = output.split('WEBSITECODE=')[1].split("&EMAILLINK")[0]
+  output = output.decode('utf-8')  
+  output = output.split('WEBSITECODE=')[1].split("&HOSTED")[0]
   html = unquote(output).strip()
 
   # make changes to the formating here 
